237950f829cd1e93f745da5c175bed1d3c296891
Nasibulin <k.nasibulin@yandex.ru>
2019-02-07 08:02:04 +0300

    -

--- src/main/java/org/springapp/entity/Product.java
+++ src/main/java/org/springapp/entity/Product.java
@@ -3,44 +3,85 @@ package org.springapp.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "category", schema = "springapp", catalog = "")
+@Table(name = "product", schema = "springapp", catalog = "")
 public class Product {
 
     @Id
     @GeneratedValue
     private int id;
 
-    @Column(name = "parent_id")
-    private int parentId;
+    @Column(name = "part_number")
+    private String partNumber;
 
-    @Column(name = "category")
-    private String category;
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "service")
+    private String service;
+
+    @Column(name = "price")
+    private double price;
+
+    private Category category;
 
     public Product() {
     }
 
-    public Product(String category) {
-        this.category=category;
+    public Product(String partNumber, String description, String service, double price) {
+        this.partNumber = partNumber;
+        this.description = description;
+        this.service = service;
+        this.price = price;
+    }
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public int getId() {
         return id;
     }
 
-    public int getParentId() {
-        return parentId;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public void setParentId(int parentId) {
-        this.parentId = parentId;
+
+    public String getPartNumber() {
+        return partNumber;
     }
 
-    public String getCategory() {
-        return category;
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
     }
 
 }
