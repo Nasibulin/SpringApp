6d2c128bf6adce71e68d11f40076bd580a8dbd66
Nasibulin <k.nasibulin@yandex.ru>
2019-02-19 20:04:14 +0300

    -

--- src/main/java/org/springapp/repository/CategoryRepository.java
+++ src/main/java/org/springapp/repository/CategoryRepository.java
@@ -16,9 +16,9 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     List<Category> findByIdEquals(Integer id);
     List<Category> findByParentIdEquals(Integer id);
     Page<Category> findByParentIdEquals(Integer i, Pageable pageable);
-    @Query(value = "SELECT category.id, category.parent_id, catname FROM (SELECT CONNECT_BY_ROOT id AS parent_id, id FROM category WHERE CONNECT_BY_ISLEAF = 1 AND id = :catid CONNECT BY   NOCYCLE PRIOR id = parent_id) t1 JOIN category ON t1.parent_id = category.id WHERE category.id > -50 ORDER BY parent_id", nativeQuery = true)
+    @Query(value = "SELECT categories.id, categories.parent_id, catname FROM (SELECT CONNECT_BY_ROOT id AS parent_id, id FROM categories WHERE CONNECT_BY_ISLEAF = 1 AND id = :catid CONNECT BY   NOCYCLE PRIOR id = parent_id) t1 JOIN categories ON t1.parent_id = categories.id WHERE categories.id > -50 ORDER BY parent_id", nativeQuery = true)
     List<Category> findCatnameById(@Param("catid") Integer catId);
-    @Query(value = "SELECT id, parent_id, catname FROM category WHERE LEVEL = :level CONNECT BY PRIOR id = parent_id START WITH   id = -1", nativeQuery = true)
+    @Query(value = "SELECT id, parent_id, catname FROM categories WHERE LEVEL = :level CONNECT BY PRIOR id = parent_id START WITH   id = -1", nativeQuery = true)
     List<Category> findCatnameByLevel(@Param("level") Integer level);
 
 }
