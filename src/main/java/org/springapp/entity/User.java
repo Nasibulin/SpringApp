758a385af5aeeddf9045f3841c03ee7b565d189b
Nasibulin <k.nasibulin@yandex.ru>
2019-03-02 15:01:44 +0300

    -

--- src/main/java/org/springapp/entity/User.java
+++ src/main/java/org/springapp/entity/User.java
@@ -8,7 +8,6 @@ import java.util.Date;
 @Table(name="users", schema = "springapp", catalog = "")
 public class User implements Serializable {
 
-    @Column(name = "user_id")
     private Integer userId;
 
     @Column(name = "email")
@@ -33,6 +32,107 @@ public class User implements Serializable {
     private Integer roleId;
 
     @Column(name = "salt")
-    private Integer salt;
+    private String salt;
 
+    public User() {
+    }
+
+    public User(Integer userId, String email, String firstName, String lastName, String passwordHash, Integer status, Date createDate, Integer roleId, String salt) {
+        this.userId = userId;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passwordHash = passwordHash;
+        this.status = status;
+        this.createDate = createDate;
+        this.roleId = roleId;
+        this.salt = salt;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_user_id_seq")
+    @SequenceGenerator(sequenceName = "users_user_id_seq", allocationSize = 1, name = "users_user_id_seq")
+    @Column(name = "user_id")
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                '}';
+    }
 }
