b8ed68c739b5975d56aba9cf095e031f603f0a69
Nasibulin <k.nasibulin@yandex.ru>
2019-03-03 18:04:03 +0300

    -

--- src/main/java/org/springapp/SecurityConfig.java
+++ src/main/java/org/springapp/SecurityConfig.java
@@ -1,11 +1,52 @@
 package org.springapp;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Konstantin
- * Date: 03.03.19
- * Time: 17:35
- * To change this template use File | Settings | File Templates.
- */
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // The pages does not require login
+        //http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/","/login", "/logout").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+
+        // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
+        // If no login, it will redirect to /login page.
+        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+
+        // For ADMIN only.
+        http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
+
+        http.formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+
+    }
+
+    @Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .userDetailsService(userService)
+//                .passwordEncoder(bcryptPasswordEncoder());
+//    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("$2a$10$35qg7pbhxFyRCkCkeyWV6.dVQtx03LFzC9UMGi4UfG90JJ.dF2OEO").roles("USER");
+    }
 }
