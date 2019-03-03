9573cba53c087e8b61d9a63ac0c068d20a0536f8
Nasibulin <k.nasibulin@yandex.ru>
2019-03-03 18:03:36 +0300

    -

---
+++ src/main/java/org/springapp/SecurityConfig.java
@@ -0,0 +1,11 @@
+package org.springapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 03.03.19
+ * Time: 17:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class SecurityConfig {
+}
