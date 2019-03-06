2be29803a017a069a39b2630df3292c2cd6ba2f1
Nasibulin <k.nasibulin@yandex.ru>
2019-03-06 12:23:25 +0300

    -

--- src/main/java/org/springapp/SecurityConfig.java
+++ src/main/java/org/springapp/SecurityConfig.java
@@ -1,22 +1,26 @@
 package org.springapp;
 
 import org.springapp.auth.service.AuthUserDetailsService;
+import org.springapp.auth.service.GuestAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private GuestAuthFilter guestAuthFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/", "/images/**", "/styles/**", "/plugins/**", "/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated();
         http.formLogin().loginPage("/login").permitAll()
