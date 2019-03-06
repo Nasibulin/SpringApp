6122a8b2142bdc37c45a62bf689887b691b98e0b
Nasibulin <k.nasibulin@yandex.ru>
2019-03-06 18:52:23 +0300

    -

--- src/main/java/org/springapp/controllers/GlobalControllerAdvice.java
+++ src/main/java/org/springapp/controllers/GlobalControllerAdvice.java
@@ -11,30 +11,36 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
 
-@ControllerAdvice
-public class GlobalControllerAdvice {
-
-    private CategoryService categoryService;
-    private ProductService productService;
-
-    @Autowired
-    public void setCategoryService(CategoryService service) {
-        this.categoryService = service;
-    }
-
-    @Autowired
-    public void setProductService(ProductService service) {
-        this.productService = service;
-    }
-
-    @ModelAttribute
-    public void globalAttributes(Model model) {
-        List<Category> topmenu = categoryService.findCatnameByLevel(2);
-        List<Category> submenu = categoryService.findCatnameByLevel(3);
-        List<Category> catname = categoryService.findCatPathById(-1);
-        model.addAttribute("catname", catname);
-        model.addAttribute("topmenu", topmenu);
-        model.addAttribute("submenu", submenu);
-    }
-
-}
+//@ControllerAdvice
+//public class GlobalControllerAdvice {
+//
+//    private CategoryService categoryService;
+//    private ProductService productService;
+//
+//    @Autowired
+//    public void setCategoryService(CategoryService service) {
+//        this.categoryService = service;
+//    }
+//
+//    @Autowired
+//    public void setProductService(ProductService service) {
+//        this.productService = service;
+//    }
+//
+//    @ModelAttribute
+//    public void globalAttributes1(Model model) {
+//        List<Category> catname = categoryService.findCatPathById(-1);
+//        model.addAttribute("catname", catname);
+//    }
+//    @ModelAttribute
+//    public void globalAttributes2(Model model) {
+//        List<Category> topmenu = categoryService.findCatnameByLevel(2);
+//        model.addAttribute("topmenu", topmenu);
+//    }
+//    @ModelAttribute
+//    public void globalAttributes3(Model model) {
+//        List<Category> submenu = categoryService.findCatnameByLevel(3);
+//        model.addAttribute("submenu", submenu);
+//    }
+//
+//}
