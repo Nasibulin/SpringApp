4b8f73a4eaa14cfac9e4d7fb4c49cda15f6ee8da
Nasibulin <k.nasibulin@yandex.ru>
2019-05-02 12:09:08 +0300

    -

--- src/main/resources/static/js/cartctrl.js
+++ src/main/resources/static/js/cartctrl.js
@@ -6,6 +6,14 @@ angular.module('myapp.cartview',[])
         // $scope.items = cart.getProducts();
         $scope.cartData = cart.getProducts();
         console.log($scope.cartData);
+        $scope.total = function () {
+            var total = 0;
+            for (var i = 0; i < $scope.cartData.length; i++) {
+                total += ($scope.cartData[i].price * $scope.cartData[i].count);
+            }
+            return total;
+        }
+
         // $scope.calculateTotal = function () {
         //     var itemList = cart.getItems();
         //     if (typeof itemList !== 'undefined') {
