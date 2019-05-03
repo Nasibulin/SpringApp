aac87d2801abc69e85b05bc07ef9aa2318d790ed
Nasibulin <k.nasibulin@yandex.ru>
2019-05-03 20:12:40 +0300

    -

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,19 +1,19 @@
 angular.module('myapp')
     .factory('ShoppingCart', ['$cookies', function ($cookies) {
 
-        var items = [];
+        var items = $cookies.getObject('cartItems');
         return {
             addProduct: function (id, partnumber, name, price) {
                 // check product already exists in cart
                 if (id) {
-                    items = $cookies.getObject('cartItems');
+                    // items = $cookies.getObject('cartItems');
                     if (items && angular.isArray(items)) {
                         for (var i = 0; i < items.length; i++) {
                             if (items[i].id === id) {
                                 // if there are quantity parameter then update quantity for product
                                 items[i].count++;
                                 // update cookie
-                                $cookies.putObject('cartItems', items);
+                                // $cookies.putObject('cartItems', items);
                                 return;
                             }
                         }
@@ -31,9 +31,9 @@ angular.module('myapp')
                         price: price
                     };
                     items.push(item);
-                    console.log(items);
+                    // console.log(items);
                     // save into cookie
-                    $cookies.putObject('cartItems', items);
+                    // $cookies.putObject('cartItems', items);
                 }
             },
             // addProduct: function (id, partnumber, name, price) {
@@ -58,20 +58,22 @@ angular.module('myapp')
                 for (var i = 0; i < items.length; i++) {
                     if (items[i].id == id) {
                         items.splice(i, 1);
-                        $cookies.remove('cartItems');
-                        $cookies.putObject('cartItems', items);
+                        // $cookies.remove('cartItems');
+                        // $cookies.putObject('cartItems', items);
                         break;
                     }
                 }
             },
 
             getProducts: function () {
-                items = $cookies.getObject('cartItems');
-                // items = [{count: 1, id: 5960, partnumber: "7600-SIP-600", name: "Cisco 7600 Series SPA Interface Processor-600", price: 112500},
-                // {count: 1, id: 5961, partnumber: "7600-SIP-600=", name: "Cisco 7600 Series SPA Interface Processor-600", price: 112500},
-                // {count: 1, id: 5964, partnumber: "SPA-OC192POS-XFP=", name: "1-port OC192/STM64 POS/RPR XFP Optics", price: 125000}]
-                return items;
-                // return cartData;
+                if (items && angular.isArray(items)) {
+                    $cookies.putObject('cartItems', items);
+                    return items;
+                }
+                else {items = [];
+                    $cookies.putObject('cartItems', items);
+                    return items;
+                }
             },
 
             clear: function () {
@@ -88,7 +90,6 @@ angular.module('myapp')
             controller: ['$scope', '$state', 'ShoppingCart', function ($scope, $state, cart) {
 
                 var items = cart.getProducts();
-                console.log(items);
                 $scope.total = function () {
                     var total = 0;
                     for (var i = 0; i < items.length; i++) {
