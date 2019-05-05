16617214bb89ea0404972d85286c93a379103c41
Nasibulin <k.nasibulin@yandex.ru>
2019-05-05 14:32:01 +0300

    -

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,6 +4,7 @@ angular.module('myapp', [
     'ui.router',
     'ngCookies',
     'myapp.products',
+    'myapp.common',
     'myapp.cart',
     'myapp.nav',
     'myapp.master',
@@ -54,6 +55,12 @@ angular.module('myapp', [
                 templateUrl: 'views/orders.html',
                 controller: 'ordersCtrl'
             })
+            // .state('orders', {
+            //     url: '/checkout',
+            //     parent: 'master',
+            //     templateUrl: 'views/checkout.html',
+            //     controller: 'checkoutCtrl'
+            // })
     }])
     .config(function ($locationProvider) {
         $locationProvider.html5Mode(true);
@@ -61,3 +68,32 @@ angular.module('myapp', [
     .config(['$compileProvider', function ($compileProvider) {
         $compileProvider.debugInfoEnabled(false);
     }])
+    .run(['$rootScope', '$location', '$cookies', '$http',
+        function ($rootScope, $location, $cookies, $http) {
+            // keep user logged in after page refresh
+            $rootScope.globals = $cookies.get('globals') || {};
+            if ($rootScope.globals.currentUser) {
+                $http.defaults.headers.common['Authorization'] = 'Bearer ' + $rootScope.globals.token;
+                $rootScope.currentUser = $rootScope.globals.currentUser;
+            }
+
+            $rootScope.isSubmitted = false;
+
+            $rootScope.$on('$locationChangeStart', function (event, next, current) {
+                console.log('received event: ' + event + ' from: ' + current + ' to go to next: ' + next);
+                // redirect to login page if not logged in and trying to access a restricted page
+                var restrictedPage = $.inArray($location.path(), ['/checkout', '/orders']) === -1;
+                var loggedIn = $rootScope.globals.currentUser;
+                $rootScope.currentUser = $rootScope.globals.currentUser;
+                if (!restrictedPage && !loggedIn) {
+                    if($location.path().indexOf('/checkout') > -1) {
+                        $location.path('/login');
+                    } else if($location.path().indexOf('orders') > -1) {
+                        $location.path('/login');
+                    } else {
+                        $location.path('/');
+                    }
+                }
+            });
+        }
+    ]);
